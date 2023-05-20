4b598c9166149509a98937020b6eaf5a397c7da9
JenselJ <saneer2002@gmail.com>
2023-05-20 12:28:25 +1000

    beginning to create flow fields

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ canvas.height = window.innerHeight;
 // canvas settings
 ctx.fillStyle = "white";
 ctx.strokeStyle = "white";
-ctx.lineWidth = 10;
+ctx.lineWidth = 2;
 ctx.lineCap = "round";
 
 class Particle {
@@ -14,9 +14,27 @@ class Particle {
     this.effect = effect;
     this.x = Math.floor(Math.random() * this.effect.width);
     this.y = Math.floor(Math.random() * this.effect.height);
+    this.speedX = Math.random() * 5 - 2.5;
+    this.speedY = Math.random() * 5 - 2.5;
+    this.history = [{ x: this.x, y: this.y }];
+    this.maxLength = 30;
   }
   draw(context) {
     context.fillRect(this.x, this.y, 10, 10);
+    context.beginPath();
+    context.moveTo(this.history[0].x, this.history[0].y);
+    for (let i = 0; i < this.history.length; i++) {
+      context.lineTo(this.history[i].x, this.history[i].y);
+    }
+    context.stroke();
+  }
+  update() {
+    this.x += this.speedX + Math.random() * 15 - 7.5;
+    this.y += this.speedY + Math.random() * 15 - 7.5;
+    this.history.push({ x: this.x, y: this.y });
+    if (this.history.length > this.maxLength) {
+      this.history.shift();
+    }
   }
 }
 
@@ -26,8 +44,16 @@ class Effect {
     this.height = height;
     this.particles = [];
     this.numberOfParticles = 50;
+    this.celSize = 20;
+    this.rows;
+    this.cols;
+    this.flowField = [];
+    this.init();
   }
   init() {
+    //create flow field
+    this.rows = Math.floor(this.height);
+
     // create particles
     for (let i = 0; i < this.numberOfParticles; i++) {
       this.particles.push(new Particle(this));
@@ -36,11 +62,17 @@ class Effect {
   render(context) {
     this.particles.forEach((particle) => {
       particle.draw(context);
+      particle.update();
     });
   }
 }
 
 const effect = new Effect(canvas.width, canvas.height);
-effect.init();
-effect.render(ctx);
 console.log(effect);
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  effect.render(ctx);
+  requestAnimationFrame(animate);
+}
+// animate();
