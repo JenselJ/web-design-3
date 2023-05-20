0d38f089be95ec7a8a7fdb6719e47099f0bbd14b
JenselJ <saneer2002@gmail.com>
2023-05-20 16:31:15 +1000

    adding timer to flow field

--- script.js
+++ script.js
@@ -14,13 +14,15 @@ class Particle {
     this.effect = effect;
     this.x = Math.floor(Math.random() * this.effect.width);
     this.y = Math.floor(Math.random() * this.effect.height);
-    this.speedX = Math.random() * 5 - 2.5;
-    this.speedY = Math.random() * 5 - 2.5;
+    this.speedX;
+    this.speedY;
+    this.speedModifier = Math.floor(Math.random() * 5 + 1);
     this.history = [{ x: this.x, y: this.y }];
-    this.maxLength = 30;
+    this.maxLength = Math.floor(Math.random() * 200 + 10);
+    this.angle = 0;
+    this.timer = this.maxLength * 2;
   }
   draw(context) {
-    context.fillRect(this.x, this.y, 10, 10);
     context.beginPath();
     context.moveTo(this.history[0].x, this.history[0].y);
     for (let i = 0; i < this.history.length; i++) {
@@ -29,12 +31,30 @@ class Particle {
     context.stroke();
   }
   update() {
-    this.x += this.speedX + Math.random() * 15 - 7.5;
-    this.y += this.speedY + Math.random() * 15 - 7.5;
-    this.history.push({ x: this.x, y: this.y });
-    if (this.history.length > this.maxLength) {
+    this.timer--;
+    if (this.timer >= 1) {
+      let x = Math.floor(this.x / this.effect.cellSize);
+      let y = Math.floor(this.y / this.effect.cellSize);
+      let index = y * this.effect.cols + x;
+      this.angle = this.effect.flowField[index];
+
+      this.speedX = Math.cos(this.angle);
+      this.speedY = Math.sin(this.angle);
+      this.x += this.speedX * this.speedModifier;
+      this.y += this.speedY * this.speedModifier;
+
+      this.history.push({ x: this.x, y: this.y });
+      if (this.history.length > this.maxLength) {
+        this.history.shift();
+      }
+    } else if (this.history.length > 1) {
       this.history.shift();
     }
+  }
+  reset() {
+    this.x = Math.floor(Math.random() * this.effect.width);
+    this.y = Math.floor(Math.random() * this.effect.height);
+    this.history = [{ x: this.x, y: this.y }];
   }
 }
 
@@ -43,16 +63,28 @@ class Effect {
     this.width = width;
     this.height = height;
     this.particles = [];
-    this.numberOfParticles = 50;
-    this.celSize = 20;
+    this.numberOfParticles = 300;
+    this.cellSize = 20;
     this.rows;
     this.cols;
     this.flowField = [];
+    this.curve = 0.5;
+    this.zoom = 0.4;
     this.init();
   }
   init() {
     //create flow field
-    this.rows = Math.floor(this.height);
+    this.rows = Math.floor(this.height / this.cellSize);
+    this.cols = Math.floor(this.width / this.cellSize);
+    this.flowField = [];
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        let angle =
+          (Math.cos(x * this.zoom) + Math.sin(y * this.zoom)) * this.curve;
+        this.flowField.push(angle);
+        console.log(this.flowField);
+      }
+    }
 
     // create particles
     for (let i = 0; i < this.numberOfParticles; i++) {
@@ -75,4 +107,4 @@ function animate() {
   effect.render(ctx);
   requestAnimationFrame(animate);
 }
-// animate();
+animate();
